23793cd3c71f98270336b8635fb16389f8851941
alexstornant <stornant.alex@gmail.com>
2024-02-01 17:32:58 -0500

    final website

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  
+  eleventyConfig.addShortcode("user", function(name, date) {
+    return `<div class="creator">
+    <p>Posted by: ${name} on ${date}</p>
+  </div>
+    `
+  });
 };
+
+
